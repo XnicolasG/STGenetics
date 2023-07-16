62f19cd5dbb308b8ee1edd411ab206eeb76c3b94
Nicolás García <nicolasg.ngl@gmail.com>
2023-07-16 12:12:37 -0500

    Create intermediate layer Business

---
+++ Business/B_Breed.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class B_Breed
+    {
+        public List<BreedEntity> BreedList()
+        {
+            using (var Db = new InventaryContext()) 
+            {
+                return Db.BreedT.ToList();
+            }
+        }
+    }
+}
